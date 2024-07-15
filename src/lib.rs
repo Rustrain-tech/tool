8dbb89b65cc878a7af4f04530ffaf3ebe5143362
rusty <codingtrain9@gmail.com>
2024-07-15 22:42:18 +0900

    genの追加

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,32 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+use itertools::Itertools;
+use rand::prelude::*;
+use rand_chacha::ChaCha20Rng;
+
+pub struct Input {
+    d: usize,
+    c: Vec<i64>,
+    s: Vec<Vec<i64>>,
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+impl std::fmt::Display for Input {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        writeln!(f, "{}", self.d)?;
+        writeln!(f, "{}", self.c.iter().join(" "))?;
+        for i in 0..self.d {
+            writeln!(f, "{}", self.s[i].iter().join(" "))?;
+        }
+        Ok(())
     }
+}
+
+pub type Output = Vec<usize>;
+
+pub fn gen(seed: u64) -> Input {
+    let mut rng = ChaCha20Rng::seed_from_u64(seed);
+    let d = 365;
+    let c = (0..26).map(|_| rng.gen_range(0..=100)).collect();
+    let s = (0..d)
+        .map(|_| (0..26).map(|_| rng.gen_range(0..=1000)).collect())
+        .collect();
+    Input { d, c, s }
 }
